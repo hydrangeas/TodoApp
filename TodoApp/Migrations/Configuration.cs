a8d82063571ac7bca5cc5f2c4b882b92de15881f
Akira Otaka <hydrangeas.project@gmail.com>
2019-12-08 11:22:41 +0900

    初期データ自動登録

--- TodoApp/Migrations/Configuration.cs
+++ TodoApp/Migrations/Configuration.cs
@@ -1,15 +1,18 @@
 namespace TodoApp.Migrations
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using TodoApp.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TodoApp.Models.TodoesContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
             ContextKey = "TodoApp.Models.TodoesContext";
         }
 
@@ -19,6 +22,44 @@ namespace TodoApp.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+
+            // Migration実行後に実行される処理
+            // 初期データを入れるのに使われる
+
+            User admin = new User()
+            {
+                Id = 1,
+                UserName = "admin",
+                Password = "password",
+                Roles = new List<Role>()
+            };
+            User user = new User()
+            {
+                Id = 2,
+                UserName = "user",
+                Password = "password",
+                Roles = new List<Role>()
+            };
+            Role administrators = new Role()
+            {
+                Id = 1,
+                RoleName = "Administrator",
+                Users = new List<User>()
+            };
+            Role users = new Role()
+            {
+                Id = 2,
+                RoleName = "User",
+                Users = new List<User>()
+            };
+
+            admin.Roles.Add(administrators);
+            administrators.Users.Add(admin);
+            user.Roles.Add(users);
+            users.Users.Add(user);
+
+            context.Users.AddOrUpdate(u => u.Id, new User[] { admin, user });
+            context.Roles.AddOrUpdate(r => r.Id, new Role[] { administrators, users });
         }
     }
 }
