6444175cc23a677e5ac16d00371e210cf7dd5884
Akira Otaka <hydrangeas.project@gmail.com>
2019-12-08 10:41:18 +0900

    表示改善、認証認可用モデル作成

---
+++ TodoApp/Models/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace TodoApp.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+
+        public string RoleName { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
