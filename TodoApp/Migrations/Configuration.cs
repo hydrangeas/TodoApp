2812d589dd8bf515bad7a895ed10a88dbc02fabb
Akira Otaka <hydrangeas.project@gmail.com>
2019-12-08 11:12:22 +0900

    Enable-Migrations実行（EF6.2へ戻さないとエラーになる）

---
+++ TodoApp/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace TodoApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TodoApp.Models.TodoesContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "TodoApp.Models.TodoesContext";
+        }
+
+        protected override void Seed(TodoApp.Models.TodoesContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
